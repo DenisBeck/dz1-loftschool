8951b528d8f9a0245105ea3e0ec65120c73d9454
DenisBeck <beckass86@gmail.com>
2016-06-17 15:48:16 +0500

    Черновой вариант верстки 4 страниц готов

--- app/js/common.js
+++ app/js/common.js
@@ -2,8 +2,9 @@
 $(document).ready(function() {
 	var container 	= $('.vision-part__container'),
 		prev		= $('.navigation-part__prev .navigation-part__wrap'),
-		next		= $('.navigation-part__next .navigation-part__wrap')
-		pics 		= $('.descr-part__img');
+		next		= $('.navigation-part__next .navigation-part__wrap'),
+		pics 		= $('.descr-part__img'),
+		aside		= $('.sidebar');
 
 		container.append(pics[1]);
 		container.find('.descr-part__img').first().removeClass('hidden');
@@ -11,4 +12,18 @@ $(document).ready(function() {
 		next.find('.descr-part__img').first().removeClass('hidden');
 		prev.append(pics[0]);
 		prev.find('.descr-part__img').first().removeClass('hidden');
+
+		$(window).on('scroll', function() {
+			
+			if (($(this).scrollTop() < aside.next().offset().top) && (document.body.clientWidth >= 1200)) {
+				aside.css('top', aside.next().offset().top - $(this).scrollTop());
+			}
+			else {
+				aside.css('top', 0);
+			}
+			
+		})
+
+		aside.find('.sidebar__item').first().addClass('sidebar__item_active')
+		
 });
