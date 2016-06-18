8a39d770964af19834a133c924b3e5aeb058ab1c
DenisBeck <beckass86@gmail.com>
2016-06-18 22:27:02 +0500

    исправления2

--- app/js/common.js
+++ app/js/common.js
@@ -1,18 +1,8 @@
 
 $(document).ready(function() {
-	var container 	= $('.vision-part__container'),
-		prev		= $('.navigation-part__prev .navigation-part__wrap'),
-		next		= $('.navigation-part__next .navigation-part__wrap'),
-		pics 		= $('.descr-part__img'),
-		aside		= $('.sidebar');
+	var aside		= $('.sidebar');
 
-		container.append(pics[1]);
-		container.find('.descr-part__img').first().removeClass('hidden');
-		next.append(pics[2]);
-		next.find('.descr-part__img').first().removeClass('hidden');
-		prev.append(pics[0]);
-		prev.find('.descr-part__img').first().removeClass('hidden');
-
+		
 		$(window).on('scroll', function() {
 			
 			if (($(this).scrollTop() < aside.next().offset().top) && (document.body.clientWidth >= 1200)) {
